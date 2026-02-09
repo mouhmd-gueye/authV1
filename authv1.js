16beb57ec7e6cb6495966eccc81ab13b1d8fa97d
mouhmd-gueye <168115598+mouhmd-gueye@users.noreply.github.com>
2026-02-09 15:32:16 +0000

    port 10000 render

--- authv1.js
+++ authv1.js
@@ -53,7 +53,7 @@ app.post('/login', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 10000
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`API démarrée sur le port ${PORT}`);
